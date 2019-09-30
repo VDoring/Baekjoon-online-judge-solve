f746b4cb3a6393cd7f3631bb786b8b933f8a557c
Doring <hojun.dev@gmail.com>
2019-09-30 17:34:43 +0900

    10430 문제

---
+++ 백준에서 가장 많이 출린 문제 TOP 100/10430.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int num1, num2, num3;
+	scanf("%d %d %d", &num1, &num2, &num3);
+
+	printf("%d\n", (num1 + num2) % num3);
+	printf("%d\n", (num1%num3 + num2 % num3) % num3);
+	printf("%d\n", (num1 * num2) % num3);
+	printf("%d", (num1%num3 * num2%num3) % num3);
+
+	return 0;
+}
