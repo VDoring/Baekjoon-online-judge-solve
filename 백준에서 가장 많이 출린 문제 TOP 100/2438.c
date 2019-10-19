5c5868fd0ee96cfcc8d41aedf3d47c41709b2e90
Doring <hojun.dev@gmail.com>
2019-10-19 11:47:09 +0900

    2438문제 해결버전

--- 백준에서 가장 많이 출린 문제 TOP 100/2438.c
+++ 백준에서 가장 많이 출린 문제 TOP 100/2438.c
@@ -1,17 +1,32 @@
+//나만의 방식
 #include <stdio.h>
 int main() {
 	int input;
 
 	scanf("%d", &input);
+	int star = input;
 
-	for (int i = input; i > 0; i--) {
-		for (int j = 0; j < i; j++) {
+	star = star - star + 1;
+	for (int i = 0; i < input; i++) {
+		for (int j = 0; j < star; j++) {
 			printf("*");
 		}
-		printf("*");
+		printf("\n");
+		star++;
+	}
+}
+
+//정답
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= i; ++j) {
+			printf("*");
+		}
+		puts("");
 	}
 	return 0;
 }
-
-// https://www.acmicpc.net/problem/2438
-// 보류중
