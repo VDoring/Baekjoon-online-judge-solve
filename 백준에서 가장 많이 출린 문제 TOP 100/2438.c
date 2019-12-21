852bb992999e22d038a4fe406a466692d86739f9
Doring <hojun.dev@gmail.com>
2019-12-21 12:21:42 +0900

    2438문제 최종

--- 백준에서 가장 많이 출린 문제 TOP 100/2438.c
+++ 백준에서 가장 많이 출린 문제 TOP 100/2438.c
@@ -1,32 +1,24 @@
-//나만의 방식
 #include <stdio.h>
 int main() {
 	int input;
-
 	scanf("%d", &input);
-	int star = input;
 
-	star = star - star + 1;
-	for (int i = 0; i < input; i++) {
-		for (int j = 0; j < star; j++) {
+	int i, j;
+	for (i = 0; i < input; i++) {
+		for (j = 0; j <= i; j++) {
 			printf("*");
 		}
 		printf("\n");
-		star++;
-	}
-}
-
-//정답
-#include <stdio.h>
-
-int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = 1; i <= n; ++i) {
-		for (int j = 1; j <= i; ++j) {
-			printf("*");
-		}
-		puts("");
 	}
 	return 0;
 }
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
