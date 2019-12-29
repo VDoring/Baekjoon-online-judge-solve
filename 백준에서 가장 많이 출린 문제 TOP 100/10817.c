a499f2d37cb1f2b8429a74cb7dd44ec45fe1e3ae
Doring <hojun.dev@gmail.com>
2019-12-29 17:59:34 +0900

    10817 문제. 정답 분석

--- 백준에서 가장 많이 출린 문제 TOP 100/10817.c
+++ 백준에서 가장 많이 출린 문제 TOP 100/10817.c
@@ -1,4 +1,38 @@
-/* 작동안됨 */
+#include <stdio.h>
+int main() {
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+
+	if (A > B) {
+		if (A > C) {
+			if (B > C) {
+				printf("%d", B); // A가 제일크고 B가 C보다 클때(A > B > C)
+			}
+			else {//((B < C)
+				printf("%d", C); // A가 제일크고 C가 B보다 클때(A > C > B)
+			}
+		}
+		else {//(A < C)
+			printf("%d", A); // A가 B보다 크나 C가 A봐 클때(C > A > B)
+		}
+	}
+	else {//(A < B)
+		if (B > C) {
+			if (A > C) {
+				printf("%d", A); // B가 제일크고 A가 C보다 클때(B > A > C)
+			}
+			else {//(A < C)
+				printf("%d", C); // B가 제일크고 A가 C보다 클때(B > A > C)
+			}
+		}
+		else {//(B < C)
+			printf("%d", B); // A가 B보다, B가 C보다 클때(A < B < C)
+		}
+	}
+}
+
+
+/* 작동안됨
 
 // 이보다 더 좋은 방법은 없다!
 // 와! if문~!
@@ -51,4 +85,4 @@ int main() {
 			return 0;
 		}
 	}
-}
+}*/
