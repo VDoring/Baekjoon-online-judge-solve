50066e451ed9577d1c1d98f6985167ccf8bee021
Doring <hojun.dev@gmail.com>
2019-12-30 15:47:59 +0900

    11718 문제. 최종

--- 백준에서 가장 많이 출린 문제 TOP 100/11718.c
+++ 백준에서 가장 많이 출린 문제 TOP 100/11718.c
@@ -1,9 +1,8 @@
-/* 작동안됨 */
-
 #include <stdio.h>
 int main() {
-	char input[150] = { 0, };
-	while (scanf("%s", input) != EOF) {
-		printf("%s\n", input);
+	char input;
+
+	while (scanf("%c", &input) != EOF) { //하나의 문자로 입력 및 EOF 체크(EOF일시 while 종료)
+		printf("%c", input); //stdin에서 글자(스페이스 포함)하나씩 꺼내와 EOF가 될때까지 출력
 	}
 }
