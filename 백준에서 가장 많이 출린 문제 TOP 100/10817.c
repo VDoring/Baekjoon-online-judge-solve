deda02247a55e18ecf2924e7328f3a9b8415a16d
Doring <hojun.dev@gmail.com>
2020-01-01 15:12:36 +0900

    10817 문제. 최종

--- 백준에서 가장 많이 출린 문제 TOP 100/10817.c
+++ 백준에서 가장 많이 출린 문제 TOP 100/10817.c
@@ -1,59 +1,41 @@
-/* 작동안됨 */
-
 #include <stdio.h>
 int main() {
-
 	/*
-	A > B > C
-	A > C > B
-	B > A > C
-	B > C > A
-	C > A > B
-	C > B > A
+	|A > B > C
+	||C > B > A
+	||B > C > A
+	|A > C > B
+	||B > A > C
+	|C > A > B
 	*/
 
-	int num1, num2, num3;
-	scanf("%d %d %d", &num1, &num2, &num3);
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
 
-	if (num2 > num1) {// B > A
-		if (num1 > num3) {//A > C
-			printf("%d", num1); // B > A > C
-		}
-		else {//C > A
-			if (num2 > num3) {//B > C
-				printf("%d", num3); // B > C > A
+	if (A > B) {
+		if (C > B) {
+			if (A > C) {
+				printf("%d", C); // A > C > B
+			}
+			else {
+				printf("%d", A); // C > A > B
 			}
 		}
-		if (num3 > num2) {//C > B
-			printf("%d", num2); // C > B > A
+		else {//B > C
+			printf("%d", B); // A > B > C
 		}
 	}
-	if (num1 > num2) {//A > B
-		if (num2 > num3) { //B > C
-			printf("%d", num2); // A > B > C
+	else {//B > A
+		if (C > B) {
+			printf("%d", B); // C > B > A
 		}
-		else { //B < C
-			printf("%d", num3); // A > C > B
-		}
-		if (num3 > num1) {//C > A
-			printf("%d", num1); // C > A > B
-		}
-	}
-	else {
-		if (num1 == num2 && num2 > num3) {
-			printf("%d", num2); // A = B > C
-		}
-		if (num2 == num3 && num1 > num2) {
-			printf("%d", num2); // A > B = C
-		}
-		if (num1 == num2 && num2 == num3) {
-			printf("%d", num2); // A = B = C
-		}
-		if (num1 == num3 && num2 > num3) {
-			printf("%d", num3); // B > A = C
-		}
-		if (num1 == num3 && num3 > num2) {
-			printf("%d", num3); // B < A = C
+		else {//B > C
+			if (A > C) {
+				printf("%d", A); // B > A > C
+			}
+			else {
+				printf("%d", C); // B > C > A
+			}
 		}
 	}
 }
