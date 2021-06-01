93d971e9c798a8c1a6ba650dc285a887d77b03a1
Doring <d0211468@kowon.dongseo.ac.kr>
2021-06-01 22:30:52 +0900

    ㅇ

---
+++ Python/2920.py
@@ -0,0 +1,17 @@
+s = input()
+if s == '1 2 3 4 5 6 7 8':
+    print('ascending')
+elif s == '8 7 6 5 4 3 2 1':
+    print('descending')
+else:
+    print('mixed')
+
+# 인터넷 답안
+a = list(map(int, input().split()))
+print(a)
+if a == sorted(a):
+    print('ascending')
+elif a == sorted(a, reverse=True):
+    print('descending')
+else:
+    print('mixed')
