1d5900d79cdffc3eefa1f81ca72f9db4066ff4fe
Doring <d0211468@kowon.dongseo.ac.kr>
2021-08-18 15:37:03 +0900

    크로아티아 알파벳

---
+++ C/2941.c
@@ -0,0 +1,38 @@
+//#include <stdio.h>
+//#include <string.h>
+//int main() {
+//	char ustr[101] = { 0, };
+//	int cnt = 0;
+//	scanf("%s", &ustr);
+//
+//	for (int i = 0; i < strlen(ustr); i++) {
+//		if ((ustr[i] == 'c' && ustr[i + 1] == '=') ||
+//			(ustr[i] == 'c' && ustr[i + 1] == '-') ||
+//			(ustr[i] == 'd' && ustr[i + 1] == '-') ||
+//			(ustr[i] == 'l' && ustr[i + 1] == 'j') ||
+//			(ustr[i] == 'n' && ustr[i + 1] == 'j') ||
+//			(ustr[i] == 's' && ustr[i + 1] == '=') ||
+//			(ustr[i] == 'z' && ustr[i + 1] == '=')) {
+//			cnt++;
+//			i += 1;
+//			continue;
+//		}
+//		else if (ustr[i] == 'd' && ustr[i + 1] == 'z' && ustr[i + 2] == '=') {
+//			cnt++;
+//			i += 2;
+//			continue;
+//		}
+//		else {
+//			cnt++;
+//		}
+//	}
+//
+//	printf("%d", cnt);
+//}
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+	char str[101];
+	
+}
